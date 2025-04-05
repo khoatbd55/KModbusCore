56848744968240afd43595efde6931fbc726772f
Khoa Tran <53319991+khoatbd55@users.noreply.github.com>
2025-04-06 00:59:07 +0700

    add linux arm transport

---
+++ KModbus/Extention/EnumExtention.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KModbus.Extention
+{
+    public static class EnumExtention
+    {
+#if NETCOREONLY
+        public static Mono.IO.Ports.StopBits Convert(this StopBits stopbit)
+        {
+            if (stopbit == StopBits.None)
+            {
+                return Mono.IO.Ports.StopBits.None;
+            }
+            else if(stopbit==StopBits.One)
+            {
+                return Mono.IO.Ports.StopBits.One;
+            }
+            else
+            {
+                return Mono.IO.Ports.StopBits.Two;
+            }
+        }
+
+        public static Mono.IO.Ports.Parity Convert(this Parity parity)
+        {
+            if(parity == Parity.None)
+            {
+                return Mono.IO.Ports.Parity.None;
+            }
+            else if(parity==Parity.Odd)
+            {
+                return Mono.IO.Ports.Parity.Odd;
+            }
+            else
+            {
+                return Mono.IO.Ports.Parity.Even;
+            }
+
+        }
+
+
+#endif
+    }
+}
